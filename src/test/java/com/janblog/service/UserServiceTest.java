c3b30675d17b38ee8b05e193a52918358ab957b0
Nelson Henrique <nhsneto@gmail.com>
2023-09-21 00:49:53 -0300

    Add shouldReturnAListOfAllUsers test

--- src/test/java/com/janblog/service/UserServiceTest.java
+++ src/test/java/com/janblog/service/UserServiceTest.java
@@ -1,10 +1,17 @@
 package com.janblog.service;
 
+import com.janblog.model.Role;
 import com.janblog.model.User;
-import org.junit.jupiter.api.Test;
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClients;
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
+import java.time.Instant;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -14,10 +21,52 @@ class UserServiceTest {
 
     @Autowired
     private UserService userService;
+    private static MongoTemplate mongoTemplate;
+    private static Logger LOG;
+
+    @BeforeAll
+    public static void initializeDb() {
+        LOG = LoggerFactory.getLogger(UserServiceTest.class);
+        LOG.info("Executing UserService tests");
+
+        String dbName = "janblog-test";
+        ConnectionString cs = new ConnectionString("mongodb://localhost:27017/" + dbName);
+        mongoTemplate = new MongoTemplate(MongoClients.create(cs), dbName);
+    }
+
+    @BeforeEach
+    public void populateDb() {
+        int NUMBER_OF_USERS = 5;
+        String[] names = {"stengerald", "ferdonanda", "danilojosef", "romanlauren", "branislavignas"};
+        String[] emails = {"stengerald@email.com", "ferdonanda@email.com", "danilojosef@email.com",
+                "romanlauren@email.com", "branislavignas@email.com"};
+
+        for (int i = 0; i < NUMBER_OF_USERS; i++) {
+            User user = new User();
+            user.setUsername(names[i]);
+            user.setEmail(emails[i]);
+            user.setPassword("1234");
+            user.setRole(Role.usr);
+            user.setCreatedAt(Instant.now());
+            user.setUpdatedAt(Instant.now());
+            mongoTemplate.insert(user);
+        }
+    }
+
+    @AfterEach
+    public void dropCollection() {
+        mongoTemplate.getCollection("users").drop();
+    }
+
+    @AfterAll
+    public static void dropDb() {
+        LOG.info("Finished executing UserService tests");
+        mongoTemplate.getDb().drop();
+    }
 
     @Test
-    public void shouldReturnAListOfAllUsers() { // Find a way to user an indepedent mongodb database for testing
+    public void shouldReturnAListOfAllUsers() {
         List<User> users = userService.findAll();
-        assertThat(users).isNotNull();
+        assertThat(users).isNotEmpty();
     }
 }
