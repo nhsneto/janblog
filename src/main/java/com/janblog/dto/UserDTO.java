1063b6f848b8ce6b52d9f63a7befbee52a472c4b
Nelson Henrique <nhsneto@gmail.com>
2023-09-24 11:55:59 -0300

    Remove messages from application.properties
    Those messages were added in the wrong place by mistake.

--- src/main/java/com/janblog/dto/UserDTO.java
+++ src/main/java/com/janblog/dto/UserDTO.java
@@ -12,17 +12,17 @@ import java.time.Instant;
 public record UserDTO(
         @JsonProperty(access = JsonProperty.Access.READ_ONLY)
         String id,
-        @NotNull(message = "{com.janblog.dto.UserDTO.username.notnull.msg}")
-        @Size(min = 6, max = 30, message = "{com.janblog.dto.UserDTO.username.size.msg}")
-        @Pattern(regexp = "^(?=.{6,30})[a-zA-Z][a-zA-Z0-9]*$", message = "{com.janblog.dto.UserDTO.username.pattern.msg}")
+        @NotNull(message = "Username must not be null")
+        @Size(min = 6, max = 30, message = "Username must be between 6 and 30 characters long")
+        @Pattern(regexp = "^(?=.{6,30})[a-zA-Z][a-zA-Z0-9]*$", message = "Username must be alphanumeric and start with a letter")
         String username,
-        @NotNull(message = "{com.janblog.dto.UserDTO.email.notnull.msg}")
+        @NotNull(message = "Email must not be null")
         @Email(regexp = "^(?=.{4,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-\\.][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*\\.[A-Za-z]{2,}$",
-                message = "{com.janblog.dto.UserDTO.email.error.msg}")
+                message = "Invalid email address. Email should be in someone@example.com format")
         String email,
         @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-        @Size(min = 8, max = 128, message = "{com.janblog.dto.UserDTO.password.size.msg}")
-        @Pattern(regexp = "^[\\p{ASCII}]{8,128}$", message = "{com.janblog.dto.UserDTO.password.pattern.msg}")
+        @Size(min = 8, max = 128, message = "Password must be between 8 and 128 characters long")
+        @Pattern(regexp = "^[\\p{ASCII}]{8,128}$", message = "Password must have ASCII characters only")
         String password,
         @JsonProperty(access = JsonProperty.Access.READ_ONLY)
         Role role,
