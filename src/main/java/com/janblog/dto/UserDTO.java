aa4488c33943543863907bc9de144823ab76248a
Nelson Henrique <nhsneto@gmail.com>
2023-09-29 14:38:28 -0300

    Remove NotNull constraint from username and email

--- src/main/java/com/janblog/dto/UserDTO.java
+++ src/main/java/com/janblog/dto/UserDTO.java
@@ -12,11 +12,9 @@ import java.time.Instant;
 public record UserDTO(
         @JsonProperty(access = JsonProperty.Access.READ_ONLY)
         String id,
-        @NotNull(message = "Username must not be null")
         @Size(min = 6, max = 30, message = "Username must be between 6 and 30 characters long")
         @Pattern(regexp = "^(?=.{6,30})[a-zA-Z][a-zA-Z0-9]*$", message = "Username must be alphanumeric and start with a letter")
         String username,
-        @NotNull(message = "Email must not be null")
         @Email(regexp = "^(?=.{4,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-\\.][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*\\.[A-Za-z]{2,}$",
                 message = "Invalid email address. Email should be in someone@example.com format")
         String email,
