b560c5438a31d87e7f01afdefcbbde8c23defe0e
Nelson Henrique <nhsneto@gmail.com>
2023-10-18 23:53:29 -0300

    Make createdAt and updatedAt fields static

--- src/main/java/com/janblog/repository/UserPopulator.java
+++ src/main/java/com/janblog/repository/UserPopulator.java
@@ -7,6 +7,7 @@ import com.mongodb.client.MongoClients;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.time.Instant;
+import java.time.OffsetDateTime;
 
 public class UserPopulator {
 
@@ -19,15 +20,15 @@ public class UserPopulator {
 
     public void populate() {
         mongoTemplate.save(new User("65135df93f90656e284ca8d8", "stengerald", "stengerald@email.com",
-                "123456", Role.usr, Instant.now(), Instant.now()));
+                "123456", Role.usr, Instant.parse("2023-10-19T02:13:12.498Z"), Instant.parse("2023-10-19T02:13:12.498Z")));
         mongoTemplate.save(new User("65135e1b3f90656e284ca8d9", "ferdonanda", "ferdonanda@email.com",
-                "123456", Role.usr, Instant.now(), Instant.now()));
+                "123456", Role.usr, Instant.parse("2023-10-19T02:13:12.503Z"), Instant.parse("2023-10-19T02:13:12.503Z")));
         mongoTemplate.save(new User("65135e273f90656e284ca8da", "danilojosef", "danilojosef@email.com",
-                "123456", Role.usr, Instant.now(), Instant.now()));
+                "123456", Role.usr, Instant.parse("2023-10-19T02:13:12.508Z"), Instant.parse("2023-10-19T02:13:12.508Z")));
         mongoTemplate.save(new User("65135e343f90656e284ca8db", "romanlauren", "romanlauren@email.com",
-                "123456", Role.usr, Instant.now(), Instant.now()));
+                "123456", Role.usr, Instant.parse("2023-10-19T02:13:12.513Z"), Instant.parse("2023-10-19T02:13:12.513Z")));
         mongoTemplate.save(new User("65135df93f90656e284ca8dc", "branislavignas", "branislavignas@email.com",
-                "123456", Role.usr, Instant.now(), Instant.now()));
+                "123456", Role.usr, Instant.parse("2023-10-19T02:13:12.518Z"), Instant.parse("2023-10-19T02:13:12.518Z")));
     }
 
     public void dropCollection() {
